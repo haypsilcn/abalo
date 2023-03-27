5d91ffe8caa8535a4d1662d5495cebc78ea78d0b
haypsilcn <yennguyen1727@gmail.com>
2023-03-27 20:09:37 +0200

    removed files

--- Aufgabe/M1/auth/web.php
+++
@@ -1,4 +0,0 @@
-<?php
-Route::get('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
-Route::get('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
-Route::get('/isloggedin', [App\Http\Controllers\AuthController::class, 'isloggedin'])->name('haslogin');
